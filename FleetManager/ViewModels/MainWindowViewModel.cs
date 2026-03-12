c131dab9418bb77f11fa2ecb2706c2417fc08cbd
karol <pokrzywnickikarol@gmail,com>
2026-03-12 09:33:45 +0100

    Created functionaliy to save list to json file

--- FleetManager/ViewModels/MainWindowViewModel.cs
+++ FleetManager/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Json;
 using FleetManager.Models;
 using FleetManager.Services;
 using ReactiveUI;
@@ -9,7 +12,10 @@ namespace FleetManager.ViewModels;
 public class MainWindowViewModel : ViewModelBase
 {
     private readonly IVehicleService _vehicleService = new JsonVehicleService();
+    
+    private static readonly JsonSerializerOptions JsonOptions = new() { WriteIndented = true };
 
+    private const string FilePath = "Assets/vehicles.json";
     public ObservableCollection<VehicleItemViewModel> Vehicles { get; } = new();
 
     private VehicleItemViewModel? _selectedVehicle;
@@ -33,4 +39,27 @@ public class MainWindowViewModel : ViewModelBase
             Vehicles.Add(new VehicleItemViewModel(vehicle));
         }
     }
+    
+    
+    private void SaveVehiclesToJson()
+    {
+        try
+        {
+            File.WriteAllText(FilePath, JsonSerializer
+                .Serialize(Vehicles, JsonOptions ));
+            Console.WriteLine("Json saved!");
+        }catch (Exception exception) when (exception is
+                                               IOException or
+                                               UnauthorizedAccessException or
+                                               JsonException)
+        
+
+        {
+            Console.WriteLine($"Save File Error {exception.Message}");
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"Unexpected error (report this!): {exception.Message}");
+        }
+    } 
 }
