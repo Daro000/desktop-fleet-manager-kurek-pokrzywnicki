33fc5e22f3491b09b69965b7cf10d81403939314
Daro <you@daro.com>
2026-03-12 08:31:13 +0100

    Creating Vehicle model

--- FleetManager/Models/Vehicle.cs
+++ FleetManager/Models/Vehicle.cs
@@ -1,6 +1,49 @@
+using ReactiveUI;
+
 namespace FleetManager.Models;
 
-public class Vehicle
+public enum VehicleStatus
 {
-    
+    Available,
+    InRoute,
+    Service
+}
+
+public class Vehicle : ReactiveObject
+{
+    private string _id = string.Empty;
+    private string _name = string.Empty;
+    private string _type = string.Empty;
+    private double _fuelPercentage;
+    private VehicleStatus _status;
+
+    public string Id
+    {
+        get => _id;
+        set => this.RaiseAndSetIfChanged(ref _id, value);
+    }
+
+    public string Name
+    {
+        get => _name;
+        set => this.RaiseAndSetIfChanged(ref _name, value);
+    }
+
+    public string Type
+    {
+        get => _type;
+        set => this.RaiseAndSetIfChanged(ref _type, value);
+    }
+
+    public double FuelPercentage
+    {
+        get => _fuelPercentage;
+        set => this.RaiseAndSetIfChanged(ref _fuelPercentage, value);
+    }
+
+    public VehicleStatus Status
+    {
+        get => _status;
+        set => this.RaiseAndSetIfChanged(ref _status, value);
+    }
 }
